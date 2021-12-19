026c89d45421cf3ec36b8e3191ba688975028055
Tyler Wilding <xtvaser@gmail.com>
2021-12-19 18:52:04 -0500

    continuing work -- mostly fixed up the navbar back to working order

--- themes/pcsx2/static/js/navbar.js
+++ themes/pcsx2/static/js/navbar.js
@@ -1,6 +1,14 @@
 $(function () {
+  $(document).ready(function () {
+    var $nav = $(".navbar.fixed-top");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $(this).height() / 4);
+    $nav.toggleClass('navbar-expand-lg', $(this).scrollTop() > $(this).height() / 4);
+    $(".navbar-brand").toggleClass('scrolled', $(this).scrollTop() > $(this).height() / 4);
+  });
   $(document).scroll(function () {
-    var $nav = $(".navbar.sticky-top");
-    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    var $nav = $(".navbar.fixed-top");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $(this).height() / 4);
+    $nav.toggleClass('navbar-expand-lg', $(this).scrollTop() > $(this).height() / 4);
+    $(".navbar-brand").toggleClass('scrolled', $(this).scrollTop() > $(this).height() / 4);
   });
 });
