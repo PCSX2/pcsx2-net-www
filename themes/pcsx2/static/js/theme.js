d3eb4c80a844ebe37cc9283f7e694d7c6cc3960f
Tyler Wilding <xtvaser@gmail.com>
2022-01-28 01:03:32 -0500

    theme: better looking dark/light theme toggle
    Closes #24

--- themes/pcsx2/static/js/theme.js
+++ themes/pcsx2/static/js/theme.js
@@ -2,16 +2,11 @@
 function setTheme(themeName) {
   localStorage.setItem('pcsx2-theme', themeName);
   document.documentElement.className = themeName;
-  if (themeName === "theme-light") {
-    $("#theme-button").html('<i class="far fa-moon theme-icon" title="Change to Dark Theme"></i>');
-  } else {
-    $("#theme-button").html('<i class="fas fa-sun theme-icon" title="Change to Light Theme"></i>');
-  }
+  $('#theme-toggle').prop('checked', themeName === "theme-light");
 }
 
-$("#theme-button").on("click", function (evt) {
-  let savedTheme = localStorage.getItem('pcsx2-theme');
-  if (savedTheme === 'theme-dark' || savedTheme === undefined) {
+$("#theme-toggle").on("change", function (evt) {
+  if (evt.target.checked) {
     setTheme('theme-light');
   } else {
     setTheme('theme-dark');
@@ -21,7 +16,7 @@ $("#theme-button").on("click", function (evt) {
 $(document).ready(function () {
   let savedTheme = localStorage.getItem('pcsx2-theme');
   if (savedTheme !== undefined && savedTheme === "theme-light") {
-    $("#theme-button").html('<i class="far fa-moon theme-icon" title="Change to Dark Theme"></i>');
+    $('#theme-toggle').prop('checked', true);
   } else {
     // Use System Theme
     if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
@@ -29,24 +24,6 @@ $(document).ready(function () {
     } else {
       setTheme('theme-light');
     }
-  }
-});
-
-// Card Animations
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add('eased-in');
-    }
-  });
-}, {
-  rootMargin: '0px',
-  threshold: 0
-});
-
-document.querySelectorAll('.card.ease-in').forEach((i) => {
-  if (i) {
-    observer.observe(i);
   }
 });
 
@@ -75,3 +52,21 @@ try {
     console.error(e2);
   }
 }
+
+// Card Animations
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('eased-in');
+    }
+  });
+}, {
+  rootMargin: '0px',
+  threshold: 0
+});
+
+document.querySelectorAll('.card.ease-in').forEach((i) => {
+  if (i) {
+    observer.observe(i);
+  }
+});
