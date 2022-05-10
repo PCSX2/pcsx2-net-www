396046bce8fb701877ad960244438d16728f4a0c
Tyler Wilding <xtvaser@gmail.com>
2022-05-09 22:30:42 -0400

    deps: switch to lodash from underscore add default chart colors

--- themes/pcsx2/static/js/chart-drawing.js
+++ themes/pcsx2/static/js/chart-drawing.js
@@ -8,10 +8,55 @@ $(".progress-chart").each(async function() {
     options = data.chartOptions;
   }
 
+  // Override Colors
+  let darkMode = isDarkMode();
+  defaultOptions = {
+    plugins: {
+      legend: {
+        labels: {
+          color: darkMode ? "#fffffe" : "#094067"
+        },
+        title: {
+          color: darkMode ? "#fffffe" : "#094067"
+        }
+      }
+    },
+    scales: {
+      x: {
+        title: {
+          color: darkMode ? "#fffffe" : "#094067"
+        },
+        grid: {
+          color: darkMode ? "#b3c2d7" : "#5f6c7b",
+          borderColor: darkMode ? "#b3c2d7" : "#5f6c7b",
+          tickColor: darkMode ? "#b3c2d7" : "#5f6c7b"
+        },
+        ticks: {
+          color: darkMode ? "#b3c2d7" : "#5f6c7b"
+        }
+      },
+      y: {
+        title: {
+          color: darkMode ? "#fffffe" : "#094067"
+        },
+        grid: {
+          color: darkMode ? "#b3c2d7" : "#5f6c7b",
+          borderColor: darkMode ? "#b3c2d7" : "#5f6c7b",
+          tickColor: darkMode ? "#b3c2d7" : "#5f6c7b"
+        },
+        ticks: {
+          color: darkMode ? "#b3c2d7" : "#5f6c7b"
+        }
+      }
+    }
+  }
+
+  const mergedOptions = _.merge(options, defaultOptions)
+
   const config = {
     type: data.type,
     data: data.chartData,
-    options: options
+    options: mergedOptions
   };
   new Chart(
     this,
