752f25d4713e08bc3b07d69b2f2ae5e9fb77db3b
Tyler Wilding <xtvaser@gmail.com>
2022-10-11 15:22:13 -0400

    migrate: finish all `devblogs`

---
+++ content/blog/2008/the-amusement-continues/index.md
@@ -0,0 +1,30 @@
+---
+title: "The Amusement Continues"
+date: 2008-02-06T00:00:00
+summary: "The new version of PCSX2 is out, development continues, ideas flow through the team and new ideas for fun spring to mind, just to forget about the work at hand for five minutes"
+draft: false
+tags:
+  - devblog
+mainAuthor: refraction
+aliases:
+  - "/developer-blog/219-the-amusement-continues"
+  - "/developer-blog/219-the-amusement-continues.html"
+  - "/developer-blog/219-the-amusement-continues.htm"
+---
+
+The new version of PCSX2 is out, development continues, ideas flow
+through the team and new ideas for fun spring to mind, just to forget
+about the work at hand for five minutes! This time, in the form of a
+video containing "misheard lyrics". We all listen to songs and sometimes
+completely miss the correct lyrics in a song, but i decided to go one
+step further with this following number and see what i could make in
+english out of a song which is sung in sweedish (afaik). The idea came
+around after Falcon4ever was thinking of ideas for a 404 page on a
+subdomain of ours to wind up one of the other team members, with the use
+of an annoying song.
+
+Well, needless to say, it worked, but the song was so crap i figured I'd
+make it a bit more fun, sat down with Cool Edit open for a while and
+jotted down what i "thought" i heard!
+
+<!-- TODO add the song, lol? -->
--- scripts/migrate-article.py
+++ scripts/migrate-article.py
@@ -3,6 +3,7 @@ import glob
 import json
 import os
 import re
+from pathlib import Path
 
 id = input("ID Prefix (ie. 219): ")
 ## [2 New Plugins for Download](/143-2-new-plugins-for-download.html) {#new-plugins-for-download .contentheading}
@@ -11,20 +12,30 @@ md_file = glob.glob('./article-backup/**/{}*.md'.format(id), recursive=True)[0]
 title = ""
 alias = ""
 content = []
+# `devblogs` have a different format unfortunately, gotta get the alias and title from the file name
+# /developer-blog/282-q-a-picking-our-noes.html
+file_name = Path(md_file).stem
+title = " ".join(file_name.split("-")[1:]).title()
+alias = "/developer-blog/{}".format(file_name)
 with open(md_file) as file:
   lines = file.readlines()
-  found_title = False
   for line in lines:
-    matches = re.findall(title_and_alias_regex, line)
-    if len(matches) > 0:
-      found_title = True
-      title = matches[0][0]
-      alias = matches[0][1]
+    if '<div class="single-article">' in line or '<div class="item-page clearfix">' in line or '[Post a Comment!]' in line:
       continue
-    if found_title:
-      if ":::" in line:
-        continue
-      content.append(line)
+    content.append(line)
+  content.insert(0, "\n")
+  # found_title = False
+  # for line in lines:
+  #   matches = re.findall(title_and_alias_regex, line)
+  #   if len(matches) > 0:
+  #     found_title = True
+  #     title = matches[0][0]
+  #     alias = matches[0][1]
+  #     continue
+  #   if found_title:
+  #     if ":::" in line:
+  #       continue
+  #     content.append(line)
 
 slug = title.replace(" ", "-").lower()
 draft = "false"
@@ -59,7 +70,7 @@ with open(pathFile, 'a') as f:
   f.write("summary: \"{}\"\n".format(summary))
   f.write("draft: {}\n".format(draft))
   f.write("tags:\n")
-  f.write("  - TODO\n")
+  f.write("  - devblog\n")
   f.write("mainAuthor: {}\n".format(author))
   f.write("aliases:\n")
   f.write("  - \"{}\"\n".format(alias))
