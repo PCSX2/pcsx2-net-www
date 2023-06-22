9344b564715555ebfdc1fe684104163fe3e22197
Tyler Wilding <xtvaser@gmail.com>
2023-06-21 20:23:18 -0400

    ci: ignore `png` check in `static/**`

--- .github/scripts/check-for-pngs.py
+++ .github/scripts/check-for-pngs.py
@@ -2,7 +2,6 @@ import glob
 
 invalid_images = glob.glob('blog/**/*.png', recursive=True)
 invalid_images += glob.glob('docs/**/*.png', recursive=True)
-invalid_images += glob.glob('static/**/*.png', recursive=True)
 
 if len(invalid_images) != 0:
   print("Found PNG files, please convert these to .webp:")
