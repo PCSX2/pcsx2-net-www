4694191dadf3456fd91c7fbce407c555185b3987
RedDevilus <martensregis@gmail.com>
2023-09-15 04:27:20 +0200

    Update index.js

--- src/components/ReleaseDownloadButton/index.js
+++ src/components/ReleaseDownloadButton/index.js
@@ -73,8 +73,7 @@ function generateDropdownItems(release, os, assets, textRemovals, isNightly) {
     } else if (os === "linux") {
       // Check for Flatpak or AppImage tags which will make Appimage - x64 Qt and Flatpak - x64 Qt and no way to seemingly fix the regular way
       if (asset.additionalTags.includes("appimage")) {
-        displayName =
-          "App" + displayName.charAt(3).toUpperCase() + displayName.slice(4); // Convert "Appimage" to "AppImage" because tags failing to uppercase it
+        displayName = "App" + displayName.charAt(3).toUpperCase() + displayName.slice(4); // Convert "Appimage" to "AppImage" because tags failing to uppercase it
       } else if (asset.additionalTags.includes("flatpak")) {
         displayName = "Flatpak";
       }
