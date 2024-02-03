019fedd7f785b72b6c9cd10e4928e4a162bd7581
Gauvain Roussel-Tarbouriech <gauvain@govanify.com>
2024-02-03 21:11:57 +0100

    BlogPostItem: add titleImage styling in prevision of 2.0

---
+++ src/theme/BlogPostItem/Header/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import BlogPostItemHeaderTitle from "@theme-original/BlogPostItem/Header/Title";
+import BlogPostItemHeaderInfo from "@theme-original/BlogPostItem/Header/Info";
+import BlogPostItemHeaderAuthors from "@theme-original/BlogPostItem/Header/Authors";
+import { useBlogPost } from "@docusaurus/theme-common/internal";
+import useBaseUrl from "@docusaurus/useBaseUrl";
+
+export default function BlogPostItemHeader() {
+  const { metadata } = useBlogPost();
+  const image = useBaseUrl(metadata.frontMatter.titleImage);
+
+  let bgStyle = {};
+  let classBlog = "";
+  if (metadata.frontMatter.titleImage) {
+    classBlog = "header-blog";
+    bgStyle = {
+      backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("${image}")`,
+    };
+  }
+  return (
+    <header className={classBlog} style={bgStyle}>
+      <BlogPostItemHeaderTitle />
+      <BlogPostItemHeaderInfo />
+      <BlogPostItemHeaderAuthors />
+    </header>
+  );
+}
