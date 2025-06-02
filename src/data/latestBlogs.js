838c87f65f1cc161e4c6639bc9bc1773f390e2e7
RedDevilus <martensregis@gmail.com>
2025-06-02 16:33:03 +0200

    Push blog be viewable

--- src/data/latestBlogs.js
+++ src/data/latestBlogs.js
@@ -2,27 +2,30 @@
 // (manually for now)
 
 import latestProgressReportImage from "/blog/2023/q1-2022-progress-report/feature-progrepq1-2022-bgonly.webp";
-import latestBlogImage from "/blog/2024/pcsx2-2-release/banner.webp";
+import latestBlogImage from "/blog/2025/pcsx2-2.4_2.2/banner.webp";
 import previousProgressReportImage from "/blog/2022/q4-2021-progress-report/feature-progrepq42021-bgonly.jpg";
-import previousBlogImage from "/blog/2022/life-is-short/feature-nobbs66.webp";
+import previousBlogImage from "/blog/2024/pcsx2-2-release/banner.webp";
 
 export const latestProgressReport = {
   url: "/blog/2023/q1-2022-progress-report",
   title: "Q1 2022 Progress Report",
   img: latestProgressReportImage,
 };
+
 export const latestBlog = {
-  url: "/blog/2024/pcsx2-2-release/",
-  title: "Introducing PCSX2 2.0!",
+  url: "/blog/2025/pcsx2-2.4_2.2/",
+  title: "Introducing PCSX2 2.4 & 2.2!",
   img: latestBlogImage,
 };
+
 export const previousProgressReport = {
   url: "/blog/2022/q4-2021-progress-report",
   title: "Q4 2021 Progress Report",
   img: previousProgressReportImage,
 };
+
 export const previousBlog = {
-  url: "/blog/2022/life-is-short",
-  title: "Life is Short",
+  url: "/blog/2024/pcsx2-2-release",
+  title: "Introducing PCSX2 2.0!",
   img: previousBlogImage,
 };
