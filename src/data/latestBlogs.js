ab73c7b4c994e4a44fd670dab6f6b9da2f9912d3
KamFretoZ <14798312+kamfretoz@users.noreply.github.com>
2025-07-01 05:19:38 +0700

    Blog: Update latest blog post list

--- src/data/latestBlogs.js
+++ src/data/latestBlogs.js
@@ -2,9 +2,9 @@
 // (manually for now)
 
 import latestProgressReportImage from "/blog/2023/q1-2022-progress-report/feature-progrepq1-2022-bgonly.webp";
-import latestBlogImage from "/blog/2024/pcsx2-2-release/banner.webp";
+import latestBlogImage from "/blog/2025/pcsx2-2.4_2.2/img/banner.webp";
 import previousProgressReportImage from "/blog/2022/q4-2021-progress-report/feature-progrepq42021-bgonly.jpg";
-import previousBlogImage from "/blog/2022/life-is-short/feature-nobbs66.webp";
+import previousBlogImage from "/blog/2024/pcsx2-2-release/banner.webp";
 
 export const latestProgressReport = {
   url: "/blog/2023/q1-2022-progress-report",
@@ -12,8 +12,8 @@ export const latestProgressReport = {
   img: latestProgressReportImage,
 };
 export const latestBlog = {
-  url: "/blog/2024/pcsx2-2-release/",
-  title: "Introducing PCSX2 2.0!",
+  url: "/blog/2025/pcsx2-2.4_2.2/",
+  title: "Welcoming PCSX2 2.2.0 and 2.4.0!",
   img: latestBlogImage,
 };
 export const previousProgressReport = {
@@ -22,7 +22,7 @@ export const previousProgressReport = {
   img: previousProgressReportImage,
 };
 export const previousBlog = {
-  url: "/blog/2022/life-is-short",
-  title: "Life is Short",
+  url: "/blog/2024/pcsx2-2-release/",
+  title: "Introducing PCSX2 2.0!",
   img: previousBlogImage,
 };
